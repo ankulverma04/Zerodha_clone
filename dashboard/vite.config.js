ad2cd379701aec06b3466492fb4199c8349c5436
Ankul Verma <ankulverma@Ankuls-MacBook-Air.local>
2026-03-21 05:33:18 +0530

    config update

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Vite SPA refresh safe config
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  server: {
+    historyApiFallback: true, // Dev me refresh safe
+  },
 })
